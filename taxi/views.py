ec46f128c9e04f9cec4357431e4b4241e865a0f4
Vova <vovakucin082@gmail.com>
2026-03-20 11:03:44 +0100

    Code write

--- taxi/views.py
+++ taxi/views.py
@@ -39,4 +39,4 @@ class DriversListView(generic.ListView):
 
 class DriverDetailView(generic.DetailView):
     model = Driver
-    queryset = Driver.objects.prefetch_related("cars")
+    queryset = Driver.objects.prefetch_related("cars__manufacturer")
